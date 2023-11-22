5799d4f9cc262b3563d468794171abe61ae392e6
Shivendu Anand <shivenduanandofficial@gmail.com>
2023-11-22 19:33:12 +0530

    Latest Changes

--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -1,56 +1,58 @@
-import s from './NewProject.module.css'
-import {useSelector} from "react-redux";
-import {Flex, Grid} from "@chakra-ui/react";
-import {useNavigate} from "react-router-dom";
+import s from "./NewProject.module.css";
+import { useSelector } from "react-redux";
+import { Flex, Grid } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import useActions from "../../hooks/useActions.js";
 import newProject from "../../assets/new.webp";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const NewProject = () => {
+  const navigate = useNavigate();
+  const projects = useSelector((state) => state.projectsReducer.projects);
+  const projectsFiltered = projects.filter(
+    (item) => item.type === "New Project"
+  );
+  const { setPickedProject } = useActions();
 
-    const navigate = useNavigate();
-    const projects = useSelector(state => state.projectsReducer.projects)
-    const projectsFiltered = projects.filter(item => item.type === 'New Project')
-    const {setPickedProject} = useActions();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
-
-    return (
-        <div className={s.projectContainer}>
-            <>
-                <div className={s.aboutImageText}>
-                    <img className={s.image} src={newProject} alt='about'/>
-                    <div
-                        className={s.textMain}
-                    >
-                        <h1>New Project</h1>
-                    </div>
-                </div>
-                {projectsFiltered.length > 0 &&
-                    <Grid className={s.grid}>
-                        {projectsFiltered.map((el, index) => (
-                            <Flex
-                                className={s.project}
-                                key={index}
-                                onClick={() => {
-                                    setPickedProject(el);
-                                    navigate(`/project/${el.title}`)
-                                }}
-                            >
-                                <img style={{width: '95%', height: '13.5rem'}}
-                                     src={`${API_URL}image/${el.title}/main/0.jpeg`}
-                                     alt={el.title}/>
-                                <h2 className={s.text}>{el.title}</h2>
-                            </Flex>
-                        ))}
-                    </Grid>
-                }
-            </>
+  return (
+    <div className={s.projectContainer}>
+      <>
+        <div className={s.aboutImageText}>
+          <img className={s.image} src={newProject} alt="about" />
+          <div className={s.textMain}>
+            <h1>New Project</h1>
+          </div>
         </div>
-    )
-}
-export default NewProject
+        {projectsFiltered.length > 0 && (
+          <Grid className={s.grid}>
+            {projectsFiltered.map((el, index) => (
+              <Flex
+                className={s.project}
+                key={index}
+                onClick={() => {
+                  setPickedProject(el);
+                  navigate(`/project/${el.title}`);
+                }}
+              >
+                <img
+                  style={{ width: "95%", height: "11.5rem" }}
+                  src={`${API_URL}image/${el.title}/main/0.jpeg`}
+                  alt={el.title}
+                />
+                <h2 className={s.text}>{el.title}</h2>
+              </Flex>
+            ))}
+            s
+          </Grid>
+        )}
+      </>
+    </div>
+  );
+};
+export default NewProject;
